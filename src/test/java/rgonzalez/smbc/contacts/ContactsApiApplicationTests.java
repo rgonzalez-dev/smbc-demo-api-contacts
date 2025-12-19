51a13b6c96c24142397d6c1727b2a666312addff
rgonzalez_pro <rafaelhome@hotmail.com>
2025-12-19 15:35:28 +0000

    first commit

---
+++ src/test/java/rgonzalez/smbc/contacts/ContactsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rgonzalez.smbc.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
