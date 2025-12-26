103af2ef0df915365991cb9fcfd16c10511f15f0
rgonzalez_pro <rafaelhome@hotmail.com>
2025-12-26 17:01:01 +0000

    matching azure free tier configuration

--- src/test/java/rgonzalez/smbc/contacts/MicroserviceTests.java
+++ src/test/java/rgonzalez/smbc/contacts/MicroserviceTests.java
@@ -2,8 +2,10 @@ package rgonzalez.smbc.contacts;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MicroserviceTests {
 
 	@Test
